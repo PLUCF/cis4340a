e418dc793caee2d4b27d18bf716e9b341cb8cc1c
PLUCF <160960688+PLUCF@users.noreply.github.com>
2024-02-23 12:14:24 -0500

    Create R15_SEC01_J.java

---
+++ SEC01-J/R15_SEC01_J.java
@@ -0,0 +1,44 @@
+// SEC01-J. Do not allow tainted variables in privileged blocks Given the non-compliant code below:
+
+private void privilegedMethod(final String filename)
+                              throws FileNotFoundException {
+  try {
+    FileInputStream fis =
+        (FileInputStream) AccessController.doPrivileged(
+          new PrivilegedExceptionAction() {
+        public FileInputStream run() throws FileNotFoundException {
+          return new FileInputStream(filename);
+        }
+      }
+    );
+    // Do something with the file and then close it
+  } catch (PrivilegedActionException e) {
+    // Forward to handler
+  }
+}
+
+// Correct the code as shown in the Compliant Solution below:
+
+private void privilegedMethod(final String filename)
+                              throws FileNotFoundException {
+  final String cleanFilename;
+  try {
+    cleanFilename = cleanAFilenameAndPath(filename);
+  } catch (/* exception as per spec of cleanAFileNameAndPath */) {
+    // Log or forward to handler as appropriate based on specification
+    // of cleanAFilenameAndPath
+  }
+  try {
+    FileInputStream fis =
+        (FileInputStream) AccessController.doPrivileged(
+          new PrivilegedExceptionAction() {
+        public FileInputStream run() throws FileNotFoundException {
+          return new FileInputStream(cleanFilename);
+        }
+      }
+    );
+    // Do something with the file and then close it
+  } catch (PrivilegedActionException e) {
+    // Forward to handler
+  }
+}
